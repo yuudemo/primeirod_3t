85f416e9a79e2f3b42222c17049857365235819f
yui <110244341+yuudemo@users.noreply.github.com>
2022-10-26 11:07:43 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,41 +1,82 @@
-
 function moeda(atual){
-return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
 }
+
 
 function total(){
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
-  let r = moeda;
-
+  let r = 0;
 
   if(!Number(c)){
-      
-   alert("O capital deve ser numeros.");
-   document.getElementById("valor").value = "";
-   document.getElementById("valor").focus();
-   return;
+    alert("hajimemashita");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
   }
 
-  if(!Number(j)){   
-    alert("O juros deve ser numeros.");
-    document.getElementById("juros").value = "";
-    document.getElementById("juros").focus();
-    return;
-   }
 
-   if(!Number(t)){
-    alert("O meses deve ser numeros.");
-    document.getElementById("meses").value = "";
-    document.getElementById("meses").focus();
-    return;
-   }
+   if(!Number(j)){
+    alert("kamisama kiss");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
   }
 
+  if(!Number(t)){
+    alert("onnako");
+    document.getElementyById("Valor").value ="";
+    document.getElementById("valor").focus();
+    return
+  }
+let mes= ""
   for(let i = 1; i <= t; i++){
       r = c * (1 + (j/100));
-      document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
-      c = moeda(r);
+      document.getElementById("mes").innerHTML="mes"+ i +"valor" ;
+     // document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
+      c = r;
   }
-  document.write("Resultado: " + moeda(r));
+  document.getElementById("mes").innerHTML=mes;
+
+  document.getElementById("total").innerHTML="Total"+moeda (r);
+  //document.write("Resultado: " + moeda(r));
+}
+
+
+function adicao(){
+let val1 = document.getElementById("v1").value;
+let val2 = document.getElementById("v2").value;
+let r = Number(val1) + Number(val2);
+document.getElementById("resultado").innerHTML =r;
+}
+
+function subtracao(){
+  let val1 = document.getElementById("v1").value;
+  let val2 = document.getElementById("v2").value;
+  let r = Number(val1) - Number(val2);
+  document.getElementById("resultado").innerHTML =r;
+  }
+
+  function divisao(){
+    let val1 = document.getElementById("v1").value;
+    let val2 = document.getElementById("v2").value;
+    let r = Number(val1) / Number(val2);
+    document.getElementById("resultado").innerHTML =r;
+    }
+  
+    function multiplicacao(){
+      let val1 = document.getElementById("v1").value;
+      let val2 = document.getElementById("v2").value;
+      let r = Number(val1) * Number(val2);
+      document.getElementById("resultado").innerHTML =r;
+      }
+      function porcentagem(){
+        let val1 = document.getElementById("v1").value;
+        let val2 = document.getElementById("v2").value;
+        let c = 100
+        let p = Number(val2)  / Number(c)
+        let r = Number(val1) * Number(p);
+        document.getElementById("resultado").innerHTML =r;
+        }
+  
