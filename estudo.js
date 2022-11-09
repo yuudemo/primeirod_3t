8afb32714cc178163baa343685cb4b1f934bbb6c
yui <110244341+yuudemo@users.noreply.github.com>
2022-11-09 10:37:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,48 +1,9 @@
-function moeda(atual){
-  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})
-}
-
-
 function total(){
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
-  let r = 0;
-
-  if(!Number(c)){
-    alert("hajimemashita");
-    document.getElementyById("Valor").value ="";
-    document.getElementById("valor").focus();
-    return
-  }
-
-
-   if(!Number(j)){
-    alert("kamisama kiss");
-    document.getElementyById("Valor").value ="";
-    document.getElementById("valor").focus();
-    return
-  }
-
-  if(!Number(t)){
-    alert("onnako");
-    document.getElementyById("Valor").value ="";
-    document.getElementById("valor").focus();
-    return
-  }
-let mes= ""
-  for(let i = 1; i <= t; i++){
-      r = c * (1 + (j/100));
-      document.getElementById("mes").innerHTML="mes"+ i +"valor" ;
-     // document.write("Mês " + i + " valor: " + moeda(r) +"<br>");
-      c = r;
-  }
-  document.getElementById("mes").innerHTML=mes;
-
-  document.getElementById("total").innerHTML="Total"+moeda (r);
-  //document.write("Resultado: " + moeda(r));
-}
-
+  let r = c;
+ 
 
 function adicao(){
 let val1 = document.getElementById("v1").value;
@@ -79,4 +40,17 @@ function subtracao(){
         let r = Number(val1) * Number(p);
         document.getElementById("resultado").innerHTML =r;
         }
-  
+        let mes = "";
+        for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+          document.getElementById("mes").innerHTML="Mês: "+moeda(r);
+          //document.write("Mês " + i + " valor: " + r +"<br>");
+          c = r;
+      }
+      document.getElementById("mes").innerHTML="Mês: "+moeda(r);
+      
+      document.getElementById("total").innerHTML="Total: "+moeda(r);
+      //document.write("Resultado: " + moeda (r);
+    }
+    
+    
